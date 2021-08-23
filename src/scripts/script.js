108bb09cf4c76565607b310e8a0aa53a4795dd7b
Nitai Charan <nitaicharan@gmail.com>
2021-08-22 22:59:07 -0300

    Finish landport

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container");
-    const containerStyle = window.getComputedStyle(container);
-    const boxImage = document.querySelector(".container--image");
-    const boxImageStyle = window.getComputedStyle(boxImage);
+// document.addEventListener("DOMContentLoaded", () => {
+//     let containerStyle = window.getComputedStyle(document.querySelector(".container"));
+//     let boxImage = document.querySelector(".top-image");
+//     let boxImageStyle = window.getComputedStyle(boxImage);
+    
+//     ChangeBoxPosition();
+    
+//     window.addEventListener("resize", ChangeBoxPosition);
 
-    const addPx = (content) => content + "px";
+//     document.querySelectorAll("dt").forEach(item => {
+//         item.addEventListener("click", UserClick);
+//     });
 
-    setBoxPosition();
+//     function UserClick(e) {
+//         if(e.target.className == "list__word") {
+//             let selectedQuestion = document.querySelector(".list__word--selected");
+            
+//             if(selectedQuestion != null) {
+//                 DisableQuestion(selectedQuestion);
+//             }
 
-    window.addEventListener("resize", setBoxPosition);
+//             EnableQuestion(e.target);
+//         }
 
-    function setBoxPosition() {
-        boxImage.style.position = "absolute";
-        boxImage.style.left = addPx(parseFloat(containerStyle.marginLeft) - 230 / 2);
-        boxImage.style.top = addPx((parseFloat(containerStyle.height) - 70) / 2);
-    }
-});
+//         else{
+//             DisableQuestion(e.target)
+//         }
+//     }
+
+//     function DisableQuestion(question){
+//         question.classList.remove("list__word--selected");
+//         question.children[1].classList.remove("flip-vertically");
+//         question.nextElementSibling.classList.remove("list__description--selected");
+//     }
+
+//     function EnableQuestion(question){
+//         question.classList.add("list__word--selected");
+//         question.children[1].classList.add("flip-vertically");
+//         question.nextElementSibling.classList.add("list__description--selected");
+//     }
+        
+//     function ChangeBoxPosition() {
+//         if(document.body.clientWidth < 768) {
+//             boxImage.style.left = (document.body.clientWidth / 2 - parseFloat(boxImageStyle.width) / 2) + "px";
+//             boxImage.style.top = "50px";
+//         }
+    
+//         else {
+//             boxImage.style.left = parseFloat(containerStyle.marginLeft) - parseFloat(boxImageStyle.width) / 2 + 10 + "px";
+//             boxImage.style.top = parseFloat(containerStyle.marginTop) + parseFloat(boxImageStyle.height) + 10 + "px"
+//         }
+//     }
+// });
